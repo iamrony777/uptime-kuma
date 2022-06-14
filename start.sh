ad3201dbd632883c2c80c97ef1fd8b29e4fdf56f
iamrony777 <kingrony777@protonmail.com>
2022-06-14 16:42:36 +0530

    Silly mistake

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Check if connected to Protonvpn
-
-exec python3 /app/protonvpn_ip_check.py
-
+python3 /app/protonvpn_ip_check.py
 
 # Then start server
-
 exec node /app/server/server.js
